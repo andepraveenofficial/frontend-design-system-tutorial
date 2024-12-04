0924258617cf0cd920128098919e458dceb37892
Ande Praveen <praveenande2023@gmail.com>
2024-12-04 10:43:55 +0530

    Form Input Implemented

--- 01-storybook-tailwindcss/src/App.tsx
+++ 01-storybook-tailwindcss/src/App.tsx
@@ -1,22 +1,39 @@
-import {Button} from "./library/components";
+import { Button } from "./library/components";
+import { Input } from "./library/form";
 import { Stack } from "./library/stack";
-import {Heading, Paragraph} from "./library/typography";
+import { Heading, Paragraph } from "./library/typography";
 
 const App = () => {
 	return (
 		<>
-			<Button onClick={() => console.log("Button clicked")} className="bg-red-500">Button</Button>
+			<Button
+				onClick={() => console.log("Button clicked")}
+				className="bg-red-500"
+			>
+				Button
+			</Button>
+			<hr />
 
 			<Heading>Heading</Heading>
+			<hr />
+
 			<Paragraph>I am Paragraph</Paragraph>
+			<hr />
+
 			<Stack>
 				<Button>Button</Button>
 				<Button>Button</Button>
 			</Stack>
-			<Stack orientation="vertical">
+			<hr />
+
+			<Stack orientation="vertical" className="block">
 				<Button>Button</Button>
 				<Button>Button</Button>
 			</Stack>
+			<hr />
+
+			<Input />
+			<hr />
 		</>
 	);
 };
